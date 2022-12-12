f27f3ad9c5b2eee302cd8f1c6f12b527d0c98afb
Wolfsurge <iantopettitt123@gmail.com>
2022-12-12 18:21:30 +0000

    Remove comment indicator note.

--- settings.py
+++ settings.py
@@ -4,7 +4,6 @@ INNER_RIGHT_BRACE = '}'
 ATTRIBUTE_LEFT_BRACE = '['
 ATTRIBUTE_RIGHT_BRACE = ']'
 
-# must be a single character
 COMMENT_INDICATOR = '~'
 
 # Completely disregards comments - does not add them to the HTML output
